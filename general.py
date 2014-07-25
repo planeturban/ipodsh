7868bc1c856512760d9c977102228c6ba4594cd6
Jonas Gulle <jonas.gulle@gmail.com>
2014-07-25 10:20:40 +0200

    Changed implementation to use the built-in hex conversion

--- general.py
+++ general.py
@@ -8,13 +8,7 @@ def mkcmd(lingo, s):
     return chr(0xff) + chr(0x55) + s + chr(sum)
 
 def toHex(s):
-    lst = []
-    for ch in s:
-        hv = hex(ord(ch)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        lst.append(hv)
-    return reduce(lambda x,y:x+y, lst)
+    return s.encode("hex")
 
 def printhex(s):
     return ":".join("{:02x}".format(ord(c)) for c in s)
